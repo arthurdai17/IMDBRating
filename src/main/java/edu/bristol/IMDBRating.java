94ef438e8899c776d572619dc098465ae827a540
Arth <tessweb97@gmail.com>
2022-03-10 01:41:02 +0000

    java_code_testing

--- src/main/java/edu/bristol/IMDBRating.java
+++ src/main/java/edu/bristol/IMDBRating.java
@@ -1,16 +1,16 @@
-package edu.bristol;
 
+package edu.bristol;
 public class IMDBRating
 {
     private float currentAverage = 0;
     private int ratingCount = 0;
-
     public float addNewRating(int newRating)
     {
         int previousTotal = (int) (currentAverage * ratingCount);
-        int newTotal = previousTotal + newRating;
-        ratingCount++;
-        currentAverage = newTotal / ratingCount;
+        int newTotal = previousTotal + newRating; //32
+        ratingCount++; //6
+        // 32/6 = 5.33F
+        currentAverage = (float) newTotal / ratingCount; // 5.714285F
         return currentAverage;
     }
 }
--- src/test/java/edu/bristol/IMDBRatingTest.java
+++ src/test/java/edu/bristol/IMDBRatingTest.java
@@ -1,7 +1,8 @@
 package edu.bristol;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class IMDBRatingTest
 {
@@ -10,11 +11,25 @@ public class IMDBRatingTest
     {
         float averageRating;
         IMDBRating rater = new IMDBRating();
-
         averageRating = rater.addNewRating(2);
         assertTrue(averageRating == 2.0, "Adding 1st rating: average should be 2.0");
 
         averageRating = rater.addNewRating(4);
         assertTrue(averageRating == 3.0, "Adding 2nd rating: average should be 3.0");
+
+        averageRating = rater.addNewRating(6);
+        assertTrue(averageRating == 4.0, "Adding 3rd rating: average should be 4.0");
+
+        averageRating = rater.addNewRating(4);
+        assertEquals(averageRating == 4.0, true);
+
+        averageRating = rater.addNewRating(9); //25
+        assertTrue(averageRating == 5.0, "Adding 5th rating: average should be 5.0");
+
+        averageRating = rater.addNewRating(7); //32
+        assertTrue(averageRating == 5.3333333333F, "Adding 6th rating: average should be 5.333.....");
+
+        averageRating = rater.addNewRating(10); //40
+        assertFalse(averageRating == 5.714285714285F, "Adding 7th rating: average should be 5.714285.....");
     }
 }
